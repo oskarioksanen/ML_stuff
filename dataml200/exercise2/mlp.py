fc58b6289aff10d1836ebd20f4add2de1aadbf39
oskarioksanen <126570043+oskarioksanen@users.noreply.github.com>
2023-03-24 15:10:52 +0200

    Muutoksia tehty, jotta nn toimii oikein

--- dataml200/exercise2/mlp.py
+++ dataml200/exercise2/mlp.py
@@ -29,7 +29,7 @@ def load_data(class_paths):
                 """
 
     data = np.array(data)
-    data = data.reshape(-1, 3, 64, 64)
+    data = data.reshape(-1, 3, 64, 64).astype("uint16")
     #print(data.shape)
     class_labels = np.array(class_labels)
 
@@ -37,11 +37,19 @@ def load_data(class_paths):
                                                                         class_labels,
                                                                         test_size=0.2,
                                                                         random_state=42)
-    train_labels = tf.keras.utils.to_categorical(train_labels, num_classes=2)
+    """plt.figure()
+    plt.imshow(train_data[0])
+    plt.show()"""
+
     """print("Training data shape:", train_data.shape)
     print("Testing data shape:", test_data.shape)
     print("Training labels shape:", train_labels.shape)
     print("Testing labels shape:", test_labels.shape)"""
+
+    #train_labels = tf.keras.utils.to_categorical(train_labels, num_classes=2)
+    #test_labels = tf.keras.utils.to_categorical(test_labels, num_classes=2)
+    train_data = train_data / 255.0
+    test_data = test_data / 255.0
 
     return train_data, test_data, train_labels, test_labels
 
@@ -54,7 +62,7 @@ def create_sequential_model():
     # 2nd layer of 100 fully-connected neurons
     model.add(tf.keras.layers.Dense(100, activation="relu"))
     # Output layer of 10 neurons
-    model.add(tf.keras.layers.Dense(2, activation="softmax"))
+    model.add(tf.keras.layers.Dense(2))
 
     return model
 
@@ -73,7 +81,7 @@ mlp_model = create_sequential_model()
 
 print(mlp_model.summary())
 mlp_model.compile(optimizer='SGD',
-                  loss=tf.keras.losses.CategoricalCrossentropy())
+                  loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True))
 history = mlp_model.fit(train_data,
                         train_labels,
                         epochs=10,
@@ -82,7 +90,9 @@ history = mlp_model.fit(train_data,
 plt.plot(history.history['loss'])
 
 pred_labels = mlp_model.predict(test_data)
-pred_labels_max = np.argmax(pred_labels)
+print(pred_labels)
+pred_labels_max = np.argmax(pred_labels, axis=1)
+print(pred_labels_max)
 test_acc = 1 - np.count_nonzero(test_labels-pred_labels_max)/len(test_labels)
 
 print(test_acc)
