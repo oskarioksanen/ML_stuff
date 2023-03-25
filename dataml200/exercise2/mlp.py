b7ed4eec6df78d6ffc5e2b44dedbae0079d5163c
oskarioksanen <126570043+oskarioksanen@users.noreply.github.com>
2023-03-25 16:42:05 +0200

    Korjauksia ja parannuksia aikasempaan

--- dataml200/exercise2/mlp.py
+++ dataml200/exercise2/mlp.py
@@ -3,6 +3,7 @@ import numpy as np
 import os
 import PIL
 from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score
 # For debugging
 import matplotlib.pyplot as plt
 
@@ -16,38 +17,25 @@ def load_data(class_paths):
             if file_name.endswith(".jpg"):
                 image_path = os.path.join(class_path, file_name)
                 image = PIL.Image.open(image_path)
-                im_array = np.array(image)
+                im_array = np.array(image, dtype=np.float32)
                 data.append(im_array)
                 class_labels.append(label)
 
-                """
-                # For debugging
-                plt.figure(1)
-                plt.clf()
-                plt.imshow(image)
-                plt.pause(0.01)
-                """
-
     data = np.array(data)
-    data = data.reshape(-1, 3, 64, 64).astype("uint16")
-    #print(data.shape)
+    data = data.reshape(-1, 3, 64, 64).astype("float32")
     class_labels = np.array(class_labels)
 
     train_data, test_data, train_labels, test_labels = train_test_split(data,
                                                                         class_labels,
                                                                         test_size=0.2,
-                                                                        random_state=42)
-    """plt.figure()
-    plt.imshow(train_data[0])
-    plt.show()"""
+                                                                        random_state=20,
+                                                                        shuffle=True)
 
-    """print("Training data shape:", train_data.shape)
-    print("Testing data shape:", test_data.shape)
-    print("Training labels shape:", train_labels.shape)
-    print("Testing labels shape:", test_labels.shape)"""
-
+    # If we use CategorialCrossentropy instead of SparseCategorialCrossentropy
+    # We need to change train labels to onehot arrays
     #train_labels = tf.keras.utils.to_categorical(train_labels, num_classes=2)
-    #test_labels = tf.keras.utils.to_categorical(test_labels, num_classes=2)
+
+    # Normalize our data
     train_data = train_data / 255.0
     test_data = test_data / 255.0
 
@@ -66,44 +54,58 @@ def create_sequential_model():
 
     return model
 
+def get_test_accuracy(test_labels, pred_labels):
+
+    """correct_pred = 0
+    test_total = 0
+    print(test_labels)
+    print(pred_labels)
+    for i in range(len(test_labels)):
+        test_total += 1
+        if pred_labels[i] == test_labels[i]:
+            correct_pred += 1
+
+    accuracy = correct_pred/test_total"""
+    # Use ready made function from scikit learning to measure the accuracy of the model
+    accuracy = accuracy_score(pred_labels, test_labels)
+
+    return accuracy
+
 # Start of main code
-data_paths = [(r'C:\Users\oksan\anaconda3\envs\dataml_200\ex2\GTSRB_subset_2\GTSRB_subset_2\class1', 0),
-               (r'C:\Users\oksan\anaconda3\envs\dataml_200\ex2\GTSRB_subset_2\GTSRB_subset_2\class2', 1)]
+data_paths = [(r'C:\Users\oksan\anaconda3\envs\tf\ex2_dataml200\GTSRB_subset_2\GTSRB_subset_2\class1', 0),
+               (r'C:\Users\oksan\anaconda3\envs\tf\ex2_dataml200\GTSRB_subset_2\GTSRB_subset_2\class2', 1)]
 
 train_data, test_data, train_labels, test_labels = load_data(data_paths)
 
+"""
+# Prints for debugging
 print("Training data shape:", train_data.shape)
 print("Testing data shape:", test_data.shape)
 print("Training labels shape:", train_labels.shape)
 print("Testing labels shape:", test_labels.shape)
+"""
 
 mlp_model = create_sequential_model()
 
 print(mlp_model.summary())
+
+# If the output of the model is normalized (output layer using softmax function for example),
+# we can set the from_logits=False
 mlp_model.compile(optimizer='SGD',
-                  loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True))
+                  loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
+                  metrics=['accuracy'])
 history = mlp_model.fit(train_data,
                         train_labels,
                         epochs=10,
-                        batch_size=100)
+                        batch_size=10)
 
 plt.plot(history.history['loss'])
 
 pred_labels = mlp_model.predict(test_data)
-print(pred_labels)
 pred_labels_max = np.argmax(pred_labels, axis=1)
-print(pred_labels_max)
-test_acc = 1 - np.count_nonzero(test_labels-pred_labels_max)/len(test_labels)
 
-print(test_acc)
+test_acc = get_test_accuracy(test_labels, pred_labels_max)
+
+print()
+print(f"Accuracy for test set: {test_acc:.02f}")
 plt.show()
-
-"""mlp_model = tf.keras.models.Sequential()
-# Flattening layer
-mlp_model.add(tf.keras.layers.Flatten(input_shape=(3, 64, 64)))
-# 1st layer of 100 fully-connected neurons
-mlp_model.add(tf.keras.layers.Dense(10, activation="relu"))
-# 2nd layer of 100 fully-connected neurons
-mlp_model.add(tf.keras.layers.Dense(10, activation="relu"))
-# Output layer of 10 neurons
-mlp_model.add(tf.keras.layers.Dense(2, activation="softmax"))"""
